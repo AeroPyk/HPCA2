839107b2737e0cb30e92c1d13e9661cb19dba0a6
AeroPyk <aeropyk@gmail.com>
2020-04-20 23:47:44 +0200

    Exercise 3

--- Exercise3.c
+++ Exercise3.c
@@ -16,7 +16,9 @@ double x[N]; // Put in the tas
 double mysecond()
 {
     struct timeval tp;
+    struct timezone tzp;
 
+    gettimeofday(&tp,&tzp);
     return ( (double) tp.tv_sec + (double) tp.tv_usec * 1.e-6 );
 }
 
